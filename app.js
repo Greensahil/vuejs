561495bc1164bbc4d294dce6216df967b59876aa
Sahil Sharma <ssharma2@svsu.edu>
2018-04-10 19:28:40 -0400

    vue js event listeners

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 new Vue({
     el:'#vue-app',
     data:{
-        name:'Sahil'
-    }
-});
+        age:24,
+        x:0,
+        y:0
+    },
+    methods:{
+        add:function(inc){
+            this.age+=inc
+        },
+        subtract:function(dec){
+            this.age-=dec;
+        },
+        updateXY:function(event){
+            this.x= event.offsetX,
+            this.y=event.offsetY
+        }
+        }
+
+        });
